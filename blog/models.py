5f6a659104cbdf3633959f23a83dbfbc9f28524b
EdaOra <eorakci22@my.uaa.k12.tr>
2018-04-28 17:55:57 +0300

    second

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-	yazar = models.ForeignKey('auth.User') 
+	yazar = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING) 
 	baslik = models.CharField(max_length=200) 
 	yazi = models.TextField() 
-	yaratilma_tarihi = models.DateTimeField(
-		default=timezone.now) 
-	yayinlanma_tarihi = models.DateTimeField(
-		blank=True, null=True)
+	yaratilma_tarihi = models.DateTimeField(default=timezone.now) 
+	yayinlanma_tarihi = models.DateTimeField(blank=True, null=True)
 
 	def yayinla(self):
 		self.yayinlanma_tarihi = timezone.now() 
